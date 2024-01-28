21591cf93643c8e9f1234bd92341ae201574c870
Eric Zhao <140433074+mykinglebron@users.noreply.github.com>
2024-01-28 14:46:18 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -5,7 +5,7 @@ import java.util.Scanner; // Import the Scanner class to read text files
 public class PigLatin {
     
     public void tester() {
-        String[] lines = {"beast","dough","happy","question","star","three","eagle","try"};
+        // String[] lines = loadStrings("words.txt");
         String[] lines = new String[8]; 
         try{
             File myFile = new File("words.txt");
@@ -27,36 +27,40 @@ public class PigLatin {
 	        System.out.println(pigLatin(lines[i]));
 	    }
     }
-   public int findFirstVowel(String sWord){
-
-if( sWord.length()>0){
-  for(int i=0; i <sWord.length(); i++){
-    if(sWord.charAt(i)=='a'||sWord.charAt(i)=='e'||sWord.charAt(i)=='i'||
-    sWord.charAt(i)=='o'||sWord.charAt(i)=='u'){
-   return i;
+    public int findFirstVowel(String sWord) {
+        //precondition: sWord is a valid String of length greater than 0.
+        //postcondition: returns the position of the first vowel in sWord.  If there are no vowels, returns -1
+	    // your code goes here
+	    
+        if ( sWord.length()>0) {
+    for (int i=0; i <sWord.length(); i++) {
+      if (sWord.charAt(i)=='a'||sWord.charAt(i)=='e'||sWord.charAt(i)=='i'||
+        sWord.charAt(i)=='o'||sWord.charAt(i)=='u') {
+        return i;
+      }
     }
   }
-}
 
-{
-  return -1;
-}
-}
+  {
+    return -1;
+  }
+    }
 
-   public String pigLatin(String sWord) {
-  int firstVowelPosition = findFirstVowel(sWord);
+    public String pigLatin(String sWord) {
+         int firstVowelPosition = findFirstVowel(sWord);
 
   if (firstVowelPosition == -1) {
-    
+    // Rule 1:if all consonants,  add "ay" to the end of the word.
     return sWord + "ay";
   } else if (firstVowelPosition == 0) {
-    
+    // Rule 2:if begin with vowels,  add "way" to the end of the word.
     return sWord + "way";
   } else if (sWord.substring(0, 2).equalsIgnoreCase("qu")) {
-   
+    // Rule 3: ifbeginning with "qu," move the "qu" to the end  and add "ay."
     return sWord.substring(2) + "quay";
   } else {
-    
+    // Rule 4: ift begin with consonants, move the leading consonants to the end and add "ay."
     return sWord.substring(firstVowelPosition) + sWord.substring(0, firstVowelPosition) + "ay";
   }
-}
+    }
+}//end PigLatin class
